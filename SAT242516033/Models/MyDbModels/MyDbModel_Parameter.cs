3102703d7c2beef97c4ad4aa60fb54f9757fae18
Ege Ertüfekçi <egeertufekci123@gmail.com>
2025-12-23 20:44:04 +0300

    Güncel

--- SAT242516033/Models/MyDbModels/MyDbModel_Parameter.cs
+++ SAT242516033/Models/MyDbModels/MyDbModel_Parameter.cs
@@ -9,26 +9,33 @@ public interface IMyDbModel_Parameter
     int TotalRecordCount { get; set; }
     IDictionary<string, object> Params { get; set; }
     IDictionary<string, string> Where { get; set; }
+    bool HasNext { get; }
+    bool HasPrevious { get; }
 }
 
-internal sealed class MyDbModel_Parameter : IMyDbModel_Parameter
+public class MyDbModel_Parameter : IMyDbModel_Parameter
 {
     public static MyDbModel_Parameter Create(int pageNumber, int pageSize, string orderBy) => new(pageNumber, pageSize, orderBy);
+    public static MyDbModel_Parameter Create() => new MyDbModel_Parameter(1, 10, "Id asc");
+
     private MyDbModel_Parameter(int pageNumber, int pageSize, string orderBy)
     {
         PageNumber = pageNumber;
         PageSize = pageSize;
         OrderBy = orderBy;
 
-        if (Params == null) Params = new Dictionary<string, object>();
-        if (Where == null) Where = new Dictionary<string, string>();
+        Params = new Dictionary<string, object>();
+        Where = new Dictionary<string, string>();
     }
 
-    public int PageNumber { get; set; }
-    public int PageSize { get; set; }
+    public int PageNumber { get; set; } = 1;
+    public int PageSize { get; set; } = 10;
     public int TotalRecordCount { get; set; }
     public int TotalPageCount => (int)Math.Ceiling(TotalRecordCount / (double)(PageSize <= 0 ? 1 : PageSize));
-    public string OrderBy { get; set; }
+    public string OrderBy { get; set; } = "Id asc";
     public IDictionary<string, object> Params { get; set; }
     public IDictionary<string, string> Where { get; set; }
+
+    public bool HasPrevious => PageNumber > 1;
+    public bool HasNext => PageNumber < TotalPageCount;
 }
